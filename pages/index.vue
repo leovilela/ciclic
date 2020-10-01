7e000113e5757a74ff528fdb8bf0c7662a0b4b98
Leo Vilela <leovila@gmail.com>
2020-09-30 23:43:37 -0300

    configuração e primeiro comit

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div  class="container-fluid">
+    <div>
+      <div class="row justify-content-center">
+        <FormPedido class="formPedido"></FormPedido>
+      </div>     
+
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import LogoPrincipal from "~/components/logoPrincipal";
+import FormPedido from "~/components/formPedido";
+
+export default {
+  
+  components: {            
+    LogoPrincipal
+  },
+  
+  computed: {        
+        ...mapState({
+            listagem: state => state.listagem
+        }),
+    },
+
+  methods: {
+
+  },
+
+  data () {
+      return {        
+      }
+    }
+
+  
+}
+</script>
+
+
+<style lang='scss'>
+  @import './style.scss';
+</style>
